661c5e03decc822e9744400cde6ca9f172a94671
Alon Jourdan <alon.jourdan@gmail.com>
2015-11-10 08:57:38 -0500

    Test assignment - not complete

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,12 @@
+def translate(phrase)
+	vowels = ['a', 'e', 'i', 'o', 'u']
+	word_array = phrase.split(" ")
+	word_array.each do |word|
+		if vowel_list.include?(word[0])
+			piglatin_string << "#{word}ay"
+		elsif 
+end
+
+end
+
+end
---
+++ 07_book_titles/book.rb
@@ -0,0 +1,5 @@
+def setup
+    @book = Book.new
+
+end
+
